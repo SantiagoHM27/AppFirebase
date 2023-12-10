3d98e0ca5cfd3325699ba4ebba15912398153529
SantiagoHM27 <himejimadarkrai@gmail.com>
2023-12-10 01:45:53 -0600

    04 Ya mero

--- app/src/main/java/com/example/myapplication/ui/AppComponents.kt
+++ app/src/main/java/com/example/myapplication/ui/AppComponents.kt
@@ -3,13 +3,18 @@
 package com.example.myapplication.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
@@ -108,26 +113,42 @@ fun TextFieldComponent(
 }
 
 @Composable
-fun AnimalCard(image: Int){
+fun AnimalCard(image: Int, selected : Boolean, animalSelected : (animalName:String) -> Unit
+){
     Card (
+        shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .padding(24.dp)
             .size(130.dp),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
+        Box(
+          modifier = Modifier
+              .fillMaxSize()
+              .border(
+                  width = 1.dp,
+                  color = if (selected) Color.Green else Color.Transparent,
+                  shape = RoundedCornerShape(8.dp)
+
+              )
+        ){
+
+
         Image(
             modifier = Modifier
                 .padding(16.dp)
                 .wrapContentWidth()
-                .wrapContentHeight(),
+                .wrapContentHeight()
+                .clickable {
+                    val animalName = if (image == R.drawable.gato2) "Cat" else "Dog"
+                    animalSelected(animalName)
+
+                },
             painter = painterResource(id = image) ,
-            contentDescription = "Animal Image" )
+            contentDescription = "Animal Image"
+        )
+    }
     }
 
 }
 
-@Preview
-@Composable
-fun AnimalCardPreview(){
-    AnimalCard(R.drawable.perro22)
-}
--- app/src/main/java/com/example/myapplication/ui/screens/UserInputScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/UserInputScreen.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +13,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,8 +66,21 @@ fun UserInputScreen(userInputViewModel: UserInputViewModel) {
 
 
             Row (modifier = Modifier.fillMaxWidth()){
-                AnimalCard(image = R.drawable.gato2)
-                AnimalCard(image = R.drawable.perro)
+                AnimalCard(image = R.drawable.gato2, animalSelected = {
+                    userInputViewModel.onEvent(
+                        UserDataUiEvents.AnimalSelected(it)
+                    )
+
+                }, selected =  userInputViewModel.uiState.value.animalSelected == "Cat")
+                AnimalCard(image = R.drawable.perro, animalSelected = {
+                    userInputViewModel.onEvent(
+                        UserDataUiEvents.AnimalSelected(it)
+                    )
+
+
+                },
+                    selected = userInputViewModel.uiState.value.animalSelected == "Dog")
+
 
             }
 
@@ -74,23 +92,46 @@ fun UserInputScreen(userInputViewModel: UserInputViewModel) {
     }
 }
 @Composable
-fun AnimalCard(image: Int){
+fun AnimalCard(image: Int, selected : Boolean, animalSelected : (animalName:String) -> Unit
+){
     Card (
+        shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .padding(24.dp)
             .size(130.dp),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Image(
+        Box(
             modifier = Modifier
-                .padding(16.dp)
-                .wrapContentWidth()
-                .wrapContentHeight(),
-            painter = painterResource(id = image) ,
-            contentDescription = "Animal Image" )
+                .fillMaxSize()
+                .border(
+                    width = 1.dp,
+                    color = if (selected) Color.Green else Color.Transparent,
+                    shape = RoundedCornerShape(8.dp)
+
+                )
+        ){
+
+
+            Image(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .wrapContentWidth()
+                    .wrapContentHeight()
+                    .clickable {
+                        val animalName = if (image == R.drawable.gato2) "Cat" else "Dog"
+                        animalSelected(animalName)
+
+                    },
+                painter = painterResource(id = image) ,
+                contentDescription = "Animal Image"
+            )
+        }
     }
 
 }
+
+
 
 @Preview
 @Composable
